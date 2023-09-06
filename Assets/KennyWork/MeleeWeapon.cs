c0e252ca95a8547ab702347e2983803717c6deba
KennyDev0213 <kedong0213@gmail.com>
2023-09-05 19:22:52 -0700

    Added some 'Kenney' assets

--- Assets/KennyWork/MeleeWeapon.cs
+++ Assets/KennyWork/MeleeWeapon.cs
@@ -11,7 +11,18 @@ public class MeleeWeapon : Weapon {
     }
     
     public override void Use() {
-        //TODO att melee functionality
+        //TODO add melee functionality
+        RaycastHit[] raycastHits = Physics.SphereCastAll(transform.position, range, transform.forward);
+        foreach (RaycastHit hit in raycastHits) {
+
+            Health targetHealth = hit.transform.GetComponent<Health>();
+
+            if(targetHealth != null) 
+            {
+                targetHealth.AddHealth(-damage);
+            }
+        }
+
         Debug.Log($"{gameObject.name} is attacking");
     }
 }
