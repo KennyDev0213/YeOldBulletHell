c513761c1b8ba2b3dd906c7f0837060fa6842805
seth.akshay478@gmail.com <seth.akshay478@gmail.com>
2023-09-08 16:19:05 -0700

    Changes to compoents and collision

--- Assets/Akshay Work/Scripts/ItemPickup.cs
+++ Assets/Akshay Work/Scripts/ItemPickup.cs
@@ -6,9 +6,9 @@ using UnityEngine.Events;
 public class ItemPickup : MonoBehaviour
 {
     public UnityEvent onItemPickup; //On Item Pick up Call the PlayerItemManager to pick up the item
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
     {
-        if (other.CompareTag("Player"))
+        if (other.collider.CompareTag("Player"))
         {
             onItemPickup?.Invoke();
             Destroy(gameObject);
