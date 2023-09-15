a9926bc82dd7627a4495824f3fc3b0ea37ff7040
seth.akshay478@gmail.com <seth.akshay478@gmail.com>
2023-09-14 19:07:38 -0700

    Finished Powerups and Edited in dev room

--- Assets/Akshay Work/Scripts/PlayerItems/PowerUpItem.cs
+++ Assets/Akshay Work/Scripts/PlayerItems/PowerUpItem.cs
@@ -2,11 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using static UnityEngine.Rendering.DebugUI;
 
 public class PowerUpItem : PlayerItem
 {
+    [SerializeField] public float _damage;
+    [SerializeField] public float _attackspeed;
+    [SerializeField] public float _movespeed;
+    [SerializeField] public float _maxhealth;
+    [SerializeField] public float _healthregen;
+
+    AttributeManager attributeManager;
+
+    private void Start()
+    {
+        attributeManager = AttributeManager.instance;
+    }
     public override void OnPickup()
     {
+        attributeManager.UpdateAttribute("player_damage_multiplier", _damage);
+        attributeManager.UpdateAttribute("player_movespeed_multiplier", _movespeed);
+        attributeManager.UpdateAttribute("player_maxhealth_multiplier", _maxhealth);
+        attributeManager.UpdateAttribute("player_healthregen_multiplier", _healthregen);
+        attributeManager.UpdateAttribute("player_attackspeed_multiplier", _attackspeed);
         Debug.Log("Powerup Pickedup!");
     }
 
