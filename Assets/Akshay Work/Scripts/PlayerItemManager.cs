243ef9bcfccb69e599ae6d1f775a66b5a51ed2bc
seth.akshay478@gmail.com <seth.akshay478@gmail.com>
2023-09-16 18:13:17 -0700

    Updated the ItemSpawning Scripts

--- Assets/Akshay Work/Scripts/PlayerItemManager.cs
+++ Assets/Akshay Work/Scripts/PlayerItemManager.cs
@@ -1,11 +1,11 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class PlayerItemManager : MonoBehaviour
 {
     private List<string> playerInventory = new List<string>();
+    [SerializeField] private List<Transform> spawnPostions;
+    [SerializeField] private List<GameObject> player_Items;
     private void DisplayPlayerItems()
     {
         //Keeps track of the players Items
@@ -45,17 +45,39 @@ public class PlayerItemManager : MonoBehaviour
         return playerInventory.Contains(item);
     }
 
-    public void SpawnItem()
+    public void SpawnItems()
     {
         //Spawns an Item in a spot on the map.
+        ShuffleList(spawnPostions);
+        for (int i = 0; i < player_Items.Count; i++)
+        {
+            int randomIndex = Random.Range(0, spawnPostions.Count);
+            Transform spawnPosition = spawnPostions[randomIndex];
+            Instantiate(player_Items[i], spawnPosition.position, spawnPosition.rotation);
 
+            // Remove the used spawn position to prevent spawning multiple objects in the same place
+            spawnPostions.RemoveAt(randomIndex);
+        }
         Debug.Log("Item has Spawned");
+    }
+
+    private void ShuffleList<T>(List<T> list)
+    {
+        int n = list.Count;
+        for (int i = 0; i < n; i++)
+        {
+            int r = i + Random.Range(0, n - i);
+            T temp = list[i];
+            list[i] = list[r];
+            list[r] = temp;
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
         DisplayPlayerItems();
+        SpawnItems();
     }
 
     // Update is called once per frame
