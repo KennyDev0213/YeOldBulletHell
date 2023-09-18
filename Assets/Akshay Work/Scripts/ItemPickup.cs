20064b173c8107576b111f55e73fb8f2918c5115
KennyDev0213 <kedong0213@gmail.com>
2023-09-18 13:41:31 -0700

    First Prototype Completed!

--- Assets/Akshay Work/Scripts/ItemPickup.cs
+++ Assets/Akshay Work/Scripts/ItemPickup.cs
@@ -8,7 +8,23 @@ public class ItemPickup : MonoBehaviour
     public UnityEvent onItemPickup; //On Item Pick up Call the PlayerItemManager to pick up the item
     private void OnCollisionEnter(Collision other)
     {
+        Debug.Log($"Collided with {other.transform.name} using collider");
         if (other.collider.CompareTag("Player"))
+        {
+            onItemPickup?.Invoke();
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit) {
+        Debug.Log($"Collided with {hit.transform.name} using character controller");
+        onItemPickup?.Invoke();
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log($"Collided with {other.transform.name} using Trigger");
+        if (other.CompareTag("Player"))
         {
             onItemPickup?.Invoke();
             Destroy(gameObject);
